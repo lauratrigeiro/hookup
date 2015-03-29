8fedea89159a114693b13e27e64b1d033a74d36a
Laura Trigeiro <lauratrigeiro@gmail.com>
2015-03-28 22:29:05 -0700

    Add routes, home, partials, redirects, is_logged_in

--- app/routes.js
+++ app/routes.js
@@ -5,23 +5,28 @@ module.exports = function(app, passport) {
 	// =====================================
 	// HOME PAGE (with login links) ========
 	// =====================================
-	app.get(subroute + '/', function(req, res) {
-		res.render('index.ejs'); // load the index.ejs file
+	app.get(subroute + '/', redirectIfLoggedIn, function(req, res) {
+		res.render('index.ejs', {
+			is_logged_in : false
+		});
 	});
 
 	// =====================================
 	// LOGIN ===============================
 	// =====================================
 	// show the login form
-	app.get(subroute + '/login', function(req, res) {
+	app.get(subroute + '/login', redirectIfLoggedIn, function(req, res) {
 
 		// render the page and pass in any flash data if it exists
-		res.render('login.ejs', { message: req.flash('loginMessage') });
+		res.render('login.ejs', { 
+			message: req.flash('loginMessage'),
+			is_logged_in : false
+		});
 	});
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : subroute + '/profile', // redirect to the secure profile section
+            successRedirect : subroute + '/home', // redirect to the secure profile section
             failureRedirect : subroute + '/login', // redirect back to the signup page if there is an error
             failureFlash : true // allow flash messages
 		}),
@@ -40,17 +45,49 @@ module.exports = function(app, passport) {
 	// SIGNUP ==============================
 	// =====================================
 	// show the signup form
-	app.get(subroute + '/signup', function(req, res) {
+	app.get(subroute + '/signup', redirectIfLoggedIn, function(req, res) {
 		// render the page and pass in any flash data if it exists
-		res.render('signup.ejs', { message: req.flash('signupMessage') });
+		res.render('signup.ejs', {
+			message: req.flash('signupMessage'),
+			is_logged_in : false
+		});
 	});
 
 	// process the signup form
 	app.post('/signup', passport.authenticate('local-signup', {
-		successRedirect : subroute + '/profile', // redirect to the secure profile section
+		successRedirect : subroute + '/home', // redirect to the secure profile section
 		failureRedirect : subroute + '/signup', // redirect back to the signup page if there is an error
 		failureFlash : true // allow flash messages
 	}));
+
+	// Site pages
+	app.get(subroute + '/home', isLoggedIn, function(req, res) {
+		res.render('home.ejs', {
+			is_logged_in : true,
+			user : req.user // get the user out of session and pass to template
+		});
+	});
+
+	app.get(subroute + '/ask', isLoggedIn, function(req, res) {
+		res.render('ask.ejs', {
+			user : req.user,
+			is_logged_in : true
+		});
+	});
+
+	app.get(subroute + '/chat', isLoggedIn, function(req, res) {
+		res.render('chat.ejs', {
+			user : req.user,
+			is_logged_in : true
+		});
+	});
+
+	app.get(subroute + '/share', isLoggedIn, function(req, res) {
+		res.render('share.ejs', {
+			user : req.user,
+			is_logged_in : true
+		});
+	});
 
 	// =====================================
 	// PROFILE SECTION =========================
@@ -59,6 +96,7 @@ module.exports = function(app, passport) {
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get(subroute + '/profile', isLoggedIn, function(req, res) {
 		res.render('profile.ejs', {
+			is_logged_in : true,
 			user : req.user // get the user out of session and pass to template
 		});
 	});
@@ -81,4 +119,13 @@ function isLoggedIn(req, res, next) {
 
 	// if they aren't redirect them to the home page
 	res.redirect(subroute + '/');
+}
+
+function redirectIfLoggedIn(req, res, next) {
+	// if user is not authenticated in the session, carry on
+	if (!req.isAuthenticated())
+		return next();
+
+	// if they are redirect them to the home page
+	res.redirect(subroute + '/home');
 }
