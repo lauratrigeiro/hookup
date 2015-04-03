e719c20754aa3c6b52703cadb638abf3f8ce494c
Laura Trigeiro <lauratrigeiro@gmail.com>
2015-04-02 23:28:45 -0700

    Add images, server side socket.io

--- app/socket_io.js
+++ app/socket_io.js
@@ -5,31 +5,67 @@ var socket_io = require('socket.io');
 function init(server) {
 	var io = socket_io.listen(server);
 
-	// implement this in db table
-	var users = {};	// username : socket
+	var chats = {};	// username : socket
 
 	io.on('connection', function(socket) {
 		console.log('a user connected');
-		socket.on('send question', function(data) { // data, callback
-			// check if user already connected
-			// add user to db connected table	-> callback(true/false)
-			socket.username = data.username;
-			users[socket.username] = socket;
-			io.emit('new message', {	// private: use specific socket.emit instead of io
-				username : socket.username,
-				message  : data.question
-			});
+
+		socket.on('user join', function(data) {
+			var chat_id = data;
+			socket.chat_id = chat_id;
+			socket.type = 'user';
+			chats[chat_id]['user'] = socket;
 		});
 
-		socket.on('send message', function(data) {
-			io.emit('new message', {
-		//		username : socket.username,
-				message  : data
-			});
+		socket.on('sexpert join', function(data) {
+			var chat_id = data.chat_id;
+			socket.chat_id = chat_id;
+			socket.type = 'sexpert';
+			chats[chat_id]['sexpert'] = socket;
+			chats[chat_id]['user'].emit('connected to sexpert', data.sexpert_id);
 		});
+
+		socket.on('user message', function(data) {
+			// check if user already connected
+			// add user to db connected table	-> callback(true/false)
+			chats[socket.chat_id]['sexpert'].emit(data);
+			// io.emit('new message', {	// private: use specific socket.emit instead of io
+			// 	username : socket.username,
+			// 	message  : data.question
+			// });
+		});
+
+		socket.on('sexpert message', function(data) {
+			chats[socket.chat_id]['user'].emit(data);
+		});
+		// 	io.emit('new message', {
+		// //		username : socket.username,
+		// 		message  : data
+		// 	});
+		// });
 
 		socket.on('disconnect', function() {
 			console.log('user disconnected'); // remove username
+			var chat_id = socket.chat_id;
+			var chat = chats[chat_id];
+			var sexpert_socket = chat.sexpert;
+			var user_socket = chat.user;
+			if (socket.type === 'user') {
+				if (sexpert_socket) {
+					sexpert_socket.emit('user disconnected');
+				}
+			} else if (socket.type === 'sexpert') {
+				if (user_socket) {
+					user_socket.emit('sexpert disconnected');
+				}
+
+				delete sexpert_socket;
+			} else {
+				return;
+			}
+
+			delete user_socket;
+			delete chats[chat_id];
 		});
 	});
 }
