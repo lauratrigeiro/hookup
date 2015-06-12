546f7b22cdafc1af09dddf525f14f0dce66999da
greggreg <greggreg@itsgreggreg.com>
2015-06-12 17:20:56 -0500

    added edit cancel

--- public/js/approve.js
+++ public/js/approve.js
@@ -37,6 +37,26 @@ $(document).ready(function() {
 			}
 		});
 	});
+	$('#stories').on('click', '.edit-button', function() {
+    $storyContainer = $(this).parents('.story-container');
+    $story = $storyContainer.find(".story");
+    content = $storyContainer.data('content');
+    $story.replaceWith("<textarea class='story-edit'>"+content+"</textarea>");
+    $(this).parents('.story-container').append(" \
+      <div class='story-additions'> \
+        I'm here to edit the story \
+        <div> \
+          <button class='save-button btn'>Save</button> \
+          <button class='cancel-button btn'>Cancel</button> \
+        </div> \
+      </div> \
+      ");
+  });
+  $('#stories').on('click', '.cancel-button', function(){
+    $storyContainer = $(this).parents('.story-container');
+    $storyContainer.find('.story-additions').remove();
+    $storyContainer.find('.story-edit').replaceWith("<div class='story'>"+$storyContainer.data("content")+"</div>");
+  });
 });
 
 function loadStories(offset) {
@@ -45,12 +65,14 @@ function loadStories(offset) {
 		url         : '/stories/unapproved?offset=' + offset,
 		success     : function(data) {
 			data.forEach(function(story) {
-				$('#stories').append('<div class="story-container">\
+				$('#stories').append('<div class="story-container" data-story_id="'+story.story_id+'" data-content="'+story.content+'">\
 	      			<div class="story">' + showReturns(story.content) + '</div>\
 	      			<div class="votes"><button type="button" class="approve-button btn" data-story_id="' +
 	      			story.story_id + '">Approve</button>\
               <button type="button" class="deny-button btn" data-story_id="' +
 	      			story.story_id + '">Deny</button>\
+              <button type="button" class="edit-button btn" data-story_id="' +
+	      			story.story_id + '">Edit</button>\
               </div>\
 	      			<div class="byline">\
 	      				<img src="/public/images/clock.png" /><span class="date">' +
