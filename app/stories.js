7058338a8a2dc83c1bf5d94e89e9cf6b0ec867e0
greggreg <greggreg@itsgreggreg.com>
2015-06-12 20:16:57 -0500

    refactored stories, removed stories_unapproved

--- app/stories.js
+++ app/stories.js
@@ -1,256 +1,170 @@
 var utils = require('./utils');
 var db = require('../config/database');
 
+// CONSTANTS
+// Possible Story Statuses
+var statuses = {
+  submitted: 0,
+  approved:  1,
+  denied:    2
+}
+
+// PUBLIC INTERFACE
+exports.statuses = statuses;
+exports.create = create_story;
+exports.approve = approve_story;
+exports.deny = deny_story;
+exports.upvote = upvote_story;
+exports.get_approved = get_approved;
+exports.get_unapproved = get_unapproved;
+exports.edit = edit_story;
 
 
-
+// PUBLIC FUNCTIONS
 function create_story(req, res) {
-	if (!req.body || !req.body.content || req.body.content.length > 500) {
-		return res.status(400).send({
-			error      : 'Request must come from a valid session and include content 500 chars or less',
-			details    : { request : req.body },
-			error_type : 'bad request'
-		});
-	}
+  if (!req.body || !req.body.content || req.body.content.length > 500) {
+    return res.status(400).send({
+      error      : 'Request must come from a valid session and include content 500 chars or less',
+      details    : { request : req.body },
+      error_type : 'bad request'
+    });
+  }
 
-	db.get_connection(function(error, conn) {
-		if (error) {
-			conn.release();
-			return res.status(502).send({
-				error      : 'database error',
-				details    : error,
-				error_type : 'database connection'
-			});
-		}
-
-		var story_id = utils.uuid();
-		var user_id = req.user.id;
-		var content = req.body.content;
-		var querystring = 'INSERT INTO stories_unapproved (story_id, user_id, content) VALUES (?, ?, ?)';
-		conn.query(querystring, [story_id, user_id, content], function(err, rows, fields) {
-			conn.release();
-			if (err) {
-				conn.release();
-				return res.status(502).send({
-					error      : 'database error',
-					details    : err,
-					error_type : 'database query'
-				});
-			}
-
-			return res.status(201).send({ story_id : story_id, content : content });
-		});
-	});
+  connect_to_db(function(conn) {
+    var story_id = utils.uuid(),
+        user_id = req.user.id,
+        content = req.body.content,
+        q = 'INSERT INTO stories_approved (story_id, user_id, content) VALUES (?, ?, ?)';
+    query(conn, q, [story_id, user_id, content], function() {
+      res.status(201).send({ story_id : story_id, content : content });
+    });
+  });
 }
 
 function approve_story(req, res) {
-	upvote(req, res, true);
+  connect_to_db(function(conn) {
+    var story_id = req.body.story_id,
+        q = 'UPDATE stories_approved SET status = ? WHERE story_id = ?';
+    query(conn, q, [statuses.approved, story_id], function(){
+      res.status(200).send();
+    });
+  });
 }
+
 function deny_story(req, res) {
-  upvote(req, res, true, true);
+  connect_to_db(function(conn) {
+    var story_id = req.body.story_id,
+        q = 'UPDATE stories_approved SET status = ? WHERE story_id = ?';
+    query(conn, q, [statuses.denied, story_id], function(){
+      res.status(200).send();
+    });
+  });
 }
 
-function user_upvote(req, res) {
-	upvote(req, res, false);
-}
+function upvote_story(req, res) {
+  if (!req.body || !req.body.story_id) {
+    return res.status(400).send({
+      error      : 'Request must include story_id',
+      details    : { request : req.body },
+      error_type : 'bad request'
+    });
+  }
 
-function upvote(req, res, approve, deny) {
-	if (!req.body || !req.body.story_id) {
-		return res.status(400).send({
-			error      : 'Request must include story_id',
-			details    : { request : req.body },
-			error_type : 'bad request'
-		});
-	}
+  connect_to_db(function(){
+    var story_id = req.body.story_id,
+        user_id = req.user.id;
 
-	db.get_connection(function(error, conn) {
-		if (error) {
-			conn.release();
-			return res.status(502).send({
-				error      : 'database error',
-				details    : error,
-				error_type : 'database connection'
-			});
-		}
-
-		var story_id = req.body.story_id;
-		var user_id = req.user.id;
-
-		var select_querystring = 'SELECT upvote_id FROM upvotes WHERE story_id = ? and user_id = ?';
-		conn.query(select_querystring, [story_id, user_id], function(err, rows, fields) {
-			if (err) {
-				conn.release();
-				return res.status(502).send({
-					error      : 'database error',
-					details    : err,
-					error_type : 'database query'
-				});
-			} else if (rows.length > 0) {
-				conn.release();
-				return res.status(400).send({
-					error      : 'User has already upvoted this story',
-					details    : { request : req.body },
-					error_type : 'bad request'
-				});
-			}
-
-
-			if (!approve) {
-				querystring = 'UPDATE stories_approved SET upvotes = upvotes + 1 WHERE story_id = ?';
-			} else {
-				querystring = 'INSERT INTO stories_approved (story_id, user_id, content, created_ts) \
-					SELECT story_id, user_id, content, created_ts from stories_unapproved \
-					WHERE story_id = ?';
-			}
-
-			conn.query(querystring, [story_id], function(err, rows, fields) {
-				if (err) {
-					conn.release();
-					return res.status(502).send({
-						error      : 'database error',
-						details    : err,
-						error_type : 'database query'
-					});
-				}
-
-      if (deny){
-        querystring = 'UPDATE stories_approved SET upvotes = 0 where story_id = ?';
-        conn.query(querystring, [story_id], function(err){
-          if (err) {
-            conn.release();
-          }
+    // Check if a user has upvoted this story yet
+    var q = 'SELECT upvote_id FROM upvotes WHERE story_id = ? and user_id = ?';
+    query(conn, q, [story_id, user_id], function(rows, fields) {
+      // If the have return 400
+      if (rows.length > 0) {
+        conn.release();
+        return res.status(400).send({
+          error      : 'User has already upvoted this story',
+          details    : { request : req.body },
+          error_type : 'bad request'
         });
       }
 
-				var upvote_id = utils.uuid();
-				var querystring = 'INSERT INTO upvotes (upvote_id, story_id, user_id) VALUES (?, ?, ?)';
-				var params = [upvote_id, story_id, user_id];
-				conn.query(querystring, params, function(err, rows, fields) {
-					if (err) {
-						conn.release();
-						return res.status(502).send({
-							error      : 'database error',
-							details    : err,
-							error_type : 'database query'
-						});
-					}
-
-					if (!approve) {
-						conn.release();
-						return res.status(201).send({ message : 'story upvoted successfully' });
-					}
-
-					querystring = 'DELETE FROM stories_unapproved WHERE story_id = ?';
-					conn.query(querystring, [story_id], function(err, rows, fields) {
-						conn.release();
-						if (err) {
-							return res.status(502).send({
-								error      : 'database error',
-								details    : err,
-								error_type : 'database query'
-							});
-						}
-
-						res.status(201).send({ message : 'story approved successfully' });
-					});
-				});
-			});
-		});
-	});
-}
-
-
-
-function get_stories(req, res, approved) {
-	db.get_connection(function(error, conn) {
-		if (error) {
-			conn.release();
-			return res.status(502).send({
-				error      : 'database error',
-				details    : error,
-				error_type : 'database connection'
-			});
-		}
-
-		if ('offset' in req.query && req.query.offset && parseInt(req.query.offset) >= 0) {
-			offset = parseInt(req.query.offset);
-		} else {
-			offset = 0;
-		}
-
-		var querystring;
-		if (approved) {
-			querystring = 'SELECT story_id, content, upvotes, UNIX_TIMESTAMP(created_ts) created \
-				FROM stories_approved WHERE upvotes > 0 ORDER BY created_ts DESC LIMIT 10 OFFSET ?';
-		} else {
-			querystring = 'SELECT story_id, content, UNIX_TIMESTAMP(created_ts) created \
-				FROM stories_unapproved ORDER BY created_ts ASC LIMIT 10 OFFSET ?';
-		}
-
-		conn.query(querystring, [offset], function(err, rows, fields) {
-			conn.release();
-			if (err) {
-				return res.status(502).send({
-					error      : 'database error',
-					details    : err,
-					error_type : 'database query'
-				});
-			}
-
-			var data = rows.map(function(row) {
-				row_data = {
-					story_id   : row.story_id,
-					content    : row.content,
-					created    : row.created
-				};
-
-				if (approved) {
-					row_data.upvotes = row.upvotes;
-				}
-
-				return row_data;
-			});
-
-			return res.status(200).send(data);
-		});
-	});
+      // Otherwise record the upvote
+      var upvote_id = utils.uuid(),
+          q = 'INSERT INTO upvotes (upvote_id, story_id, user_id) VALUES (?, ?, ?)';
+      query(conn, q, [upvote_id, story_id, user_id], function(rows, fields) {
+        return res.status(200).send();
+      });
+    });
+  });
 }
 
 function get_approved(req, res) {
-	get_stories(req, res, true);
+  get_stories(req, res, statuses.approved);
 }
 
 function get_unapproved(req, res) {
-	get_stories(req, res, false);
+  get_stories(req, res, statuses.submitted);
 }
 
 function edit_story(req, res) {
   var story_id = req.body.story_id;
   var content = req.body.content;
-
   connect_to_db(function(conn){
-    var querystring = 'UPDATE stories_unapproved SET content = ? WHERE story_id = ?';
-
-    conn.query(querystring, [content, story_id], function(err, rows, fields) {
-      if (err) return generic_query_error(err, conn);
-    });
-    return res.status(200).send({});
+    var q = 'UPDATE stories_approved SET content = ? WHERE story_id = ?';
+    query(conn, q, [content, story_id], function() {});
+    return res.status(200).send();
   });
 }
 
+// PRIVATE FUNCTIONS
+function get_stories(req, res, story_status) {
+  connect_to_db(function(conn) {
 
+    if ('offset' in req.query && req.query.offset && parseInt(req.query.offset) >= 0) {
+      offset = parseInt(req.query.offset);
+    } else {
+      offset = 0;
+    }
+
+    var q = '\
+          SELECT story_id, content, upvotes, UNIX_TIMESTAMP(created_ts) created \
+          FROM stories_approved WHERE status = ? ORDER BY created_ts DESC \
+          LIMIT 10 OFFSET ?';
+
+    query(conn, q, [story_status, offset], function(rows, fields) {
+      var data = rows.map(function(row) {
+        return row_data = {
+          story_id   : row.story_id,
+          content    : row.content,
+          created    : row.created,
+          upvotes    : row.upvotes
+        };
+      });
+
+      return res.status(200).send(data);
+    });
+  });
+}
+// Boilerplate
+// Eventually pull this out into a library
 function connect_to_db(callback){
-	db.get_connection(function(error, conn) {
-		if (error) {
-			conn.release();
-			return res.status(502).send({
-				error      : 'database error',
-				details    : error,
-				error_type : 'database connection'
-			});
-		} else {
+  db.get_connection(function(error, conn) {
+    if (error) {
+      conn.release();
+      return res.status(502).send({
+        error      : 'database error',
+        details    : error,
+        error_type : 'database connection'
+      });
+    } else {
       callback(conn);
     }
+  });
+}
+
+function query(conn, q, params, callback){
+  conn.query(q, params, function(error, rows, fields){
+    if(error) return generic_query_error(err, conn);
+    callback(rows, fields);
   });
 }
 
@@ -263,10 +177,4 @@ function generic_query_error(err,conn){
   });
 }
 
-exports.create = create_story;
-exports.approve = approve_story;
-exports.deny = deny_story;
-exports.upvote = user_upvote;
-exports.get_approved = get_approved;
-exports.get_unapproved = get_unapproved;
-exports.edit = edit_story;
+
